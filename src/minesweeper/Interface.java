ebc26ca78bfed4eb1389aa4eef52d812537f999a
Josh Galofaro <galo9664@fredonia.edu>
2011-09-23 14:45:21 -0400

    Switched from flow layout to grid bag layout.

--- src/minesweeper/Interface.java
+++ src/minesweeper/Interface.java
@@ -2,6 +2,8 @@ package minesweeper;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -11,15 +13,38 @@ import javax.swing.JTextField;
 
 public class Interface extends JFrame {
 	
-	public static void addComponents(Container contentPane)
+	public static void addComponents(Container thePane)
 	{
-		contentPane.setLayout(new FlowLayout());
+		thePane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		thePane.add((new JLabel("Grid 0x0")), c);
+		
+		JButton button = new JButton("Button 1x0");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 2;
+		c.gridx = 1;
+		c.gridy = 0;
+		thePane.add(button, c);
+		
+		JTextField text = new JTextField("THE TEXT");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = 3;
+		c.ipady = 40;
+		c.gridx = 0;
+		c.gridy = 1;
+		thePane.add(text, c);
+		
+		/**
 		contentPane.add(new JLabel("Label A"));
 		contentPane.add(new JButton("Button A"));
         contentPane.add(new JCheckBox("JCheckBox 3"));
         contentPane.add(new JTextField("Long-Named JTextField 4"));
         contentPane.add(new JButton("JButton 5"));
+        **/
 	}
 	
 	/**
